5141f85597c1867e5b71e3ee101af00d6530fd0e
Keifer Chiang <15503711+KeiferC@users.noreply.github.com>
2019-03-25 20:43:43 +0100

    started parser

---
+++ xmlparser.py
@@ -0,0 +1,26 @@
+#
+# xmlparser.py - converts given XML file to CSV file
+#
+# Usage:  python xmlparser.py [-c <category_tag>] -r <row_tag>
+#                <input_file> [-o <output_file>]
+
+import sys
+
+# Main - Command Line
+def main():
+        # TODO: command line arguments
+
+# Parse
+
+
+# Search
+
+
+# Get row
+
+
+# File Write
+
+
+# Usage failure
+
