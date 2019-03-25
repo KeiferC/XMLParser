ba68728f5cca8cf1374a19dda4a3499bc6e193b7
Keifer Chiang <15503711+KeiferC@users.noreply.github.com>
2019-03-25 21:40:33 +0100

    finished command arg parsing

--- xmlparser.py
+++ xmlparser.py
@@ -8,8 +8,35 @@ import sys
 
 # Main - Command Line
 def main():
-        # TODO: command line arguments
-        usage()
+        line_list = []
+        row_tag = None
+        cat_tag = None
+        out_file = None
+        arg_len = len(sys.argv)
+        i = 1
+
+        # Command Line Parsing
+        if (arg_len < 9) and (arg_len > 3) and (arg_len % 2 == 0):
+                while i < arg_len:
+                        if (sys.argv[i] == '-r') and (i + 1 < arg_len):
+                                row_tag = str(sys.argv[i + 1])
+                                i += 1
+                        elif (sys.argv[i] == '-c') and (i + 1 < arg_len):
+                                cat_tag = str(sys.argv[i + 1])
+                                i += 1
+                        elif (sys.argv[i] == '-o') and (i + 1 < arg_len):
+                                out_file = str(sys.argv[i + 1])
+                                i += 1
+                        else: # TODO: check for file type
+                                line_list = [line.rstrip('\n') for 
+                                             line in open(sys.argv[i])]
+                        
+                        i += 1
+        else:
+                usage()
+
+        # debug:
+        print('\n'.join(line_list))
 
 # Parse
 
